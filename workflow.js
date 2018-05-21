44a08481b6b7a69d46af9781fb14a9d7bb3e6a93
brothers.jim <brothers.jim@gmail.com>
2018-05-21 12:28:54 -0400

    Add minifier link

--- workflow.js
+++ workflow.js
@@ -1,31 +1,8 @@
-    $(document).ready(function () {
-
-      $(".verifyC").hide();
-      $(".verifyP").hide();
-      $(".1").show();
-
-      $(".verifyP").click(function () {
-        $(".verifyC").hide(100);
-        $(this).children().first().show(100);
-      });
-
-      $(".verifyC").click(function () {
-        $(this).remove();
-        $(this).next().show(100);
-      });
-
-      $(".editC").hide();
-      $(".1").show();
-
-      $(".editP").click(function () {
-        $(".editC").hide(100);
-        $(this).children().first().show(100);
-      });
-
-      $(".editC").click(function () {
-        $(this).remove();
-        $(this).next().show(100);
-      });
-
-
-    });
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
