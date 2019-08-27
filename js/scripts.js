214ba8c9a3cc6b16dcfa749aafa77307331fe486
jimkel97 <8008538+jimkel97@users.noreply.github.com>
2019-08-26 20:12:32 -0400

    vue play

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,8 @@
 //Add a new boolean data property `onSale` and create a computed property that takes `brand`, `product` and `onSale` and prints out a string whenever `onSale` is true.
 
 var app = new Vue({
-        el: '#app',
-        data: {
-                // categories: ["main", "webdev", "learning", "misc"]
-                categories: [
-                        ["main", "urlMain"], ["webdev", "urlWebdev"]
-                ]
-        },
+    el: '#app',
+    data: {
+        // categories: ["main", "webdev", "learning", "misc"]
+    },
 })
